efc6da6d300f31cdf1933d6506b378769b607c15
NiumXp <formigacomcaimbra@gmail.com>
2022-02-08 03:29:28 -0300

    add header

--- basecamp/c-04/ex03/ft_atoi.c
+++ basecamp/c-04/ex03/ft_atoi.c
@@ -1,47 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 03:27:44 by nximenes          #+#    #+#             */
+/*   Updated: 2022/02/08 03:28:25 by nximenes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int ft_is_digit(char c)
+int	ft_is_digit(char c)
 {
-    return ('0' <= c && c <= '9');
+	return ('0' <= c && c <= '9');
 }
 
-int ft_isdigit(char c)
+int	ft_isdigit(char c)
 {
-    return (c - 48);
+	return (c - 48);
 }
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    int number;
-    int is_negative;
+	int	number;
+	int	is_negative;
 
-    number = 0;
-    while (*str == ' ')
-    {
-        ++str;
-    }
-    is_negative = *str == '-';
-    if (is_negative || *str == '+')
-        ++str;
-    while (*str)
-    {
-        if (!ft_is_digit(*str))
-            break ;
-        number *= 10;
-        if (is_negative)
-            number -= ft_isdigit(*str);
-        else
-            number += ft_isdigit(*str);
-        ++str;
-    }
-    return (number);
-}
-
-void    main(void)
-{
-    char    str[11] = "-2147483648";
-    int     num;
-
-    num = ft_atoi(str);
-    printf("%d\n", num);
+	number = 0;
+	while (*str == ' ')
+	{
+		++str;
+	}
+	is_negative = *str == '-';
+	if (is_negative || *str == '+')
+		++str;
+	while (*str)
+	{
+		if (!ft_is_digit(*str))
+			break ;
+		number *= 10;
+		if (is_negative)
+			number -= ft_isdigit(*str);
+		else
+			number += ft_isdigit(*str);
+		++str;
+	}
+	return (number);
 }
