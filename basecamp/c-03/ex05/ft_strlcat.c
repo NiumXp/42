fb3a402dee34b9d67d74f526cb3c454b1dd89ec9
NiumXp <formigacomcaimbra@gmail.com>
2022-02-15 10:32:13 -0300

    last retry in c3

--- basecamp/c-03/ex05/ft_strlcat.c
+++ basecamp/c-03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 08:27:00 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/12 04:37:31 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/02/15 10:30:55 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,24 @@ t_uint8	ft_strlen(char *str)
 	return (length);
 }
 
-char	*ft_strcpy(char *dest, char *src, t_uint8 count)
-{
-	char	*tmp;
-	char	*s;
-
-	s = src;
-	tmp = dest;
-	while (count--)
-		*tmp++ = *s++;
-	return (dest);
-}
-
 t_uint8	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	t_uint8	dsize;
-	t_uint8	len;
-	t_uint8	res;
+	t_uint8	ssize;
+	t_uint8	index;
 
+	ssize = ft_strlen(src);
+	if (size < 1)
+		return (ssize);
 	dsize = ft_strlen(dest);
-	len = ft_strlen(src);
-	res = dsize + len;
-	dest += dsize;
-	size -= dsize;
-	if (len >= size)
-		len = size - 1;
-	ft_strcpy(dest, src, len);
-	dest[len] = 0;
-	return (res);
+	if (size < dsize)
+		return (ssize + dsize);
+	index = 0;
+	while (src[index] && (index + dsize) < (size - 1))
+	{
+		dest[index + dsize] = src[index];
+		index++;
+	}
+	dest[index + dsize] = 0;
+	return (dsize + ssize);
 }
