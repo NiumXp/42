08ffe1d1c33e5e8a79e579abc198595fbca9ad91
NiumXp <formigacomcaimbra@gmail.com>
2022-02-15 07:08:30 -0300

    retry c-3

--- basecamp/c-03/ex04/ft_strstr.c
+++ basecamp/c-03/ex04/ft_strstr.c
@@ -6,44 +6,30 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 03:25:26 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/09 20:41:51 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/02/15 06:53:34 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	int	r;
-
-	while (n > 0 && (*s1 || *s2))
-	{
-		r = (*s1++ - *s2++);
-		if (r)
-			return (r);
-		--n;
-	}
-	return (0);
-}
-
-unsigned int	ft_strlen(char *str)
-{
-	unsigned int	length;
-
-	length = 0;
-	while (*str++)
-		++length;
-	return (length);
-}
+typedef unsigned int	t_uint;
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	unsigned int	length;
+	t_uint	index;
+	t_uint	anchor;
 
-	length = ft_strlen(to_find);
-	while (*str)
+	if (!*to_find)
+		return (str);
+	index = 0;
+	while (str[index])
 	{
-		if (*to_find == *str && !ft_strncmp(to_find, str, length))
-			return (str);
-		++str;
+		anchor = 0;
+		while (str[index + anchor] == to_find[anchor])
+		{
+			if (to_find[anchor + 1] == '\0')
+				return (str);
+			++anchor;
+		}
+		++index;
 	}
 	return (0);
 }
--- basecamp/c-03/ex05/ft_strlcat.c
+++ basecamp/c-03/ex05/ft_strlcat.c
@@ -6,13 +6,15 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 08:27:00 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/11 19:34:23 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/02/12 04:37:31 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(char *str)
+typedef unsigned int	t_uint8;
+
+t_uint8	ft_strlen(char *str)
 {
-	unsigned int	length;
+	t_uint8	length;
 
 	length = 0;
 	while (*str++)
@@ -20,20 +22,32 @@ unsigned int	ft_strlen(char *str)
 	return (length);
 }
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+char	*ft_strcpy(char *dest, char *src, t_uint8 count)
 {
-	unsigned int	index;
-	unsigned int	length;
-	int				number;
+	char	*tmp;
+	char	*s;
 
-	index = 0;
-	length = ft_strlen(dest);
-	number = size - length;
-	while (src[index] && number > 1)
-	{
-		dest[index + length] = src[index];
-		++index;
-		--number;
-	}
-	return (length + size - index);
+	s = src;
+	tmp = dest;
+	while (count--)
+		*tmp++ = *s++;
+	return (dest);
+}
+
+t_uint8	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	t_uint8	dsize;
+	t_uint8	len;
+	t_uint8	res;
+
+	dsize = ft_strlen(dest);
+	len = ft_strlen(src);
+	res = dsize + len;
+	dest += dsize;
+	size -= dsize;
+	if (len >= size)
+		len = size - 1;
+	ft_strcpy(dest, src, len);
+	dest[len] = 0;
+	return (res);
 }
