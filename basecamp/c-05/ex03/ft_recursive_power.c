4e0396f4737c3d020231154f4d0524dd3b127b4b
NiumXp <formigacomcaimbra@gmail.com>
2022-02-17 14:03:04 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- basecamp/c-05/ex03/ft_recursive_power.c
+++ basecamp/c-05/ex03/ft_recursive_power.c
@@ -6,12 +6,14 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 22:00:10 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/10 14:44:47 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/02/17 14:01:34 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
+	if (power < 0)
+		return (0);
 	if (power == 0)
 		return (1);
 	return (nb * ft_recursive_power(nb, power - 1));
