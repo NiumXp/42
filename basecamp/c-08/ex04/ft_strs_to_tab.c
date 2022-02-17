5262d87397d66c3a83ee3d0d53d9174332a8345a
NiumXp <formigacomcaimbra@gmail.com>
2022-02-17 19:53:10 -0300

    fix strs_to_tab

--- basecamp/c-08/ex04/ft_strs_to_tab.c
+++ basecamp/c-08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 05:13:54 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/17 05:59:22 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/02/17 19:46:33 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,51 +44,25 @@ char	*ft_strdup(char *src)
 	return (ptr);
 }
 
-void	*ft_free_strs(t_stock_str	*strs, t_uint size)
-{
-	while (size > 0)
-	{
-		free(strs[size].copy);
-		free(&strs[size]);
-	}
-	free(strs);
-	return (NULL);
-}
-
-t_stock_str	*ft_empty_stock(void)
-{
-	t_stock_str	*stock;
-
-	stock = (t_stock_str *)malloc(sizeof(t_stock_str));
-	stock->str = 0;
-	stock->size = 0;
-	stock->copy = 0;
-	return (stock);
-}
-
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
+	int			index;
 	t_stock_str	*strs;
-	t_uint		index;
-	t_stock_str	*str;
 
+	strs = NULL;
 	if (ac < 1)
-		return (NULL);
+		return (strs);
 	index = 0;
 	strs = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
 	if (strs == NULL)
-		return (NULL);
-	while (index < (t_uint)ac)
+		return (strs);
+	while (index < ac)
 	{
-		str = (t_stock_str *)malloc(sizeof(t_stock_str));
-		if (str == NULL)
-			return (ft_free_strs(strs, index));
-		str->size = ft_strsize(av[index]) - 1;
-		str->copy = ft_strdup(av[index]);
-		str->str = av[index];
-		strs[index] = *str;
+		strs[index].size = ft_strsize(av[index]) - 1;
+		strs[index].copy = ft_strdup(av[index]);
+		strs[index].str = av[index];
 		++index;
 	}
-	strs[index] = *ft_empty_stock();
+	strs[index] = (t_stock_str){0, 0, 0};
 	return (strs);
 }
