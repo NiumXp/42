8b35b2ce60ae919d50764a3e2665c7ba04cce21c
NiumXp <formigacomcaimbra@gmail.com>
2022-02-21 07:04:21 -0300

    ft_is_sort.c

--- basecamp/c-11/ex04/ft_is_sort.c
+++ basecamp/c-11/ex04/ft_is_sort.c
@@ -6,10 +6,27 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 21:44:57 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/20 22:00:33 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/02/21 06:30:58 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define TRUE 1
+#define FALSE 0
+
+typedef unsigned int	t_uint;
+
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
+	t_uint	index;
+	int		right;
+
+	index = 0;
+	while (index < (length - 1))
+	{
+		right = tab[index + 1];
+		if (f(tab[index], right) == -1)
+			return (FALSE);
+		++index;
+	}
+	return (TRUE);
 }
