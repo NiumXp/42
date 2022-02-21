91fc5c483d14d4f9a6dfefe290486424a25553dc
NiumXp <formigacomcaimbra@gmail.com>
2022-02-20 22:00:55 -0300

    c11 ex03

--- basecamp/c-11/ex04/ft_is_sort.c
+++ basecamp/c-11/ex04/ft_is_sort.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*   ft_is_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/17 07:18:59 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/20 21:43:11 by nximenes         ###   ########.fr       */
+/*   Created: 2022/02/20 21:44:57 by nximenes          #+#    #+#             */
+/*   Updated: 2022/02/20 22:00:33 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef unsigned int	t_uint;
-
-int	ft_count_if(char **tab, int length, int (*f)(char*))
+int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
-	t_uint	count;
-	t_uint	index;
-
-	count = 0;
-	index = 0;
-	while (index < length)
-	{
-		if (f(tab[index]) != 0)
-			++count;
-		++index;
-	}
-	return (count);
 }
