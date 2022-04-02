3cc8198cf41f9ee71a90a0f03e68dc8259f6b2b6
NiumXp <formigacomcaimbra@gmail.com>
2022-04-02 01:51:14 -0300

    change how to call function

--- basecamp/c-12/ex09/ft_list_foreach.c
+++ basecamp/c-12/ex09/ft_list_foreach.c
@@ -6,7 +6,7 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 01:05:26 by nximenes          #+#    #+#             */
-/*   Updated: 2022/04/02 01:08:07 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/04/02 01:50:52 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
 	while (begin_list != NULL)
 	{
-		f(begin_list->data);
+		(*f)(begin_list->data);
 		begin_list = begin_list->next;
 	}
 }
