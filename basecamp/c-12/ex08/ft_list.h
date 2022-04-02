eebcdf6a42fb10231ceaba625286191e1ca1b930
NiumXp <formigacomcaimbra@gmail.com>
2022-04-02 00:58:38 -0300

    c-12 ex08

--- basecamp/c-12/ex08/ft_list.h
+++ basecamp/c-12/ex08/ft_list.h
@@ -1,37 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/30 03:21:44 by nximenes          #+#    #+#             */
-/*   Updated: 2022/04/02 00:57:27 by nximenes         ###   ########.fr       */
+/*   Created: 2022/03/30 02:40:01 by nximenes          #+#    #+#             */
+/*   Updated: 2022/03/30 02:40:02 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_list.h"
+#ifndef FT_LIST_H
+# define FT_LIST_H
 
-#define ZERO 0
-#define NULL (void *)ZERO
-
-void	ft_list_reverse(t_list **begin_list)
+typedef struct s_list
 {
-	t_list	*list;
-	t_list	*last;
-	t_list	*temp;
+	void			*data;
+	struct s_list	*next;
+}	t_list;
 
-	last = NULL;
-	list = *begin_list;
-	if (list == NULL)
-		return ;
-	while (list->next)
-	{
-		temp = list->next;
-		list->next = last;
-		last = list;
-		list = temp;
-	}
-	list->next = last;
-	*begin_list = list;
-}
+#endif
