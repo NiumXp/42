ea222404ee106e23968c9dfadecc36f6f35cc40c
NiumXp <formigacomcaimbra@gmail.com>
2022-04-02 05:54:54 -0300

    fix write and ft_strlen prototype in ft_putstr.c

--- basecamp/c-09/ex00/ft_putstr.c
+++ basecamp/c-09/ex00/ft_putstr.c
@@ -6,11 +6,15 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 05:20:35 by nximenes          #+#    #+#             */
-/*   Updated: 2022/02/14 05:23:45 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/04/02 05:50:36 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 #define STDOUT 1
+
+int	ft_strlen(char *str);
 
 void	ft_putstr(char *str)
 {
