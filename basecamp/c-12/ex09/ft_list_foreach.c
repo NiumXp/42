be299115dc9c6186f32eefa8b70a5596ae330e02
NiumXp <formigacomcaimbra@gmail.com>
2022-04-02 01:08:58 -0300

    c-12 ex09

---
+++ basecamp/c-12/ex09/ft_list_foreach.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 01:05:26 by nximenes          #+#    #+#             */
+/*   Updated: 2022/04/02 01:08:07 by nximenes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+#define ZERO 0
+#define NULL (void *)ZERO
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list != NULL)
+	{
+		f(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
