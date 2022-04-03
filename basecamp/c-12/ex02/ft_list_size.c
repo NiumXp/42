0e50aaf8748b11379cfc7869ee6c0c657faea844
NiumXp <formigacomcaimbra@gmail.com>
2022-04-03 04:50:08 -0300

    fix ft_list_size

--- basecamp/c-12/ex02/ft_list_size.c
+++ basecamp/c-12/ex02/ft_list_size.c
@@ -6,18 +6,22 @@
 /*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 02:43:43 by nximenes          #+#    #+#             */
-/*   Updated: 2022/03/30 02:44:50 by nximenes         ###   ########.fr       */
+/*   Updated: 2022/04/03 04:49:56 by nximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <malloc.h>		// NULL
 #include "ft_list.h"
 
-int	ft_list_size(t_list *begin_list)
+int	ft_list_size(t_list	*begin_list)
 {
 	int	size;
 
 	size = 0;
-	while (begin_list->next)
+	while (begin_list != NULL)
+	{
+		begin_list = begin_list->next;
 		++size;
+	}
 	return (size);
 }
