b2f81408ade1fe643cfe76dc0e5596d143da3f1a
NiumXp <formigacomcaimbra@gmail.com>
2022-05-29 13:43:31 -0300

    add ft_calloc

---
+++ projects/basics/libft/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 21:23:17 by nximenes          #+#    #+#             */
+/*   Updated: 2022/05/24 21:29:27 by nximenes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr)
+		ft_bzero(ptr, count * size);
+	return (ptr);
+}
