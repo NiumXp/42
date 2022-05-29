97b2f3259911bd0accd5d08c3dd3b82589669bb5
NiumXp <formigacomcaimbra@gmail.com>
2022-05-29 13:46:09 -0300

    add libft.h

---
+++ projects/basics/libft/libft.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nximenes <nximenes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 21:01:41 by nximenes          #+#    #+#             */
+/*   Updated: 2022/05/25 14:11:31 by nximenes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# define TRUE 1
+
+# include <stddef.h>	// size_t, NULL
+# include <malloc.h>	// malloc
+# include <stdint.h>
+
+int		_is_in_range(int e, int min, int max);
+
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_strlen(const char *s);
+int		ft_strncmp(const char *lhs, const char *rhs, size_t count);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
